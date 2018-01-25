ead851c5642bcf7256559961933d37be950bdf1e
Royston Yinkore <roystonyinkore@gmail.com>
2018-01-25 16:56:22 +0000

    Login view

--- src/app/view/index.js
+++ src/app/view/index.js
@@ -1,21 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import './index.css';
+import 'bootstrap';
+import './style.css';
+ 
+import {view as LoginView} from '../features/login';
 
 class App extends Component {  
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          Hello world
-        </p>
-      </div>
-    );
+      return(<LoginView />);
   }
 }
 
